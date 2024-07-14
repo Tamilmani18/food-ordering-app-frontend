234283bbcd72d63021d3610738d54e9136a42834
Tamil <Tamilmani.tm018@gmail.com>
2024-07-14 08:34:22 +0530

    added search page and filters

---
+++ src/components/SortOptionDropdown.tsx
@@ -0,0 +1,55 @@
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+type Props = {
+  onChange: (value: string) => void;
+  sortOption: string;
+};
+
+const SORT_OPTIONS = [
+  {
+    lable: "Best match",
+    value: "bestMatch",
+  },
+  {
+    lable: "Delivery price",
+    value: "deliveryPrice",
+  },
+  {
+    lable: "Estimated delivery time",
+    value: "estimatedDeliveryTime",
+  },
+];
+
+const SortOptionDropdown = ({ onChange, sortOption }: Props) => {
+  const selectedSortLabel =
+    SORT_OPTIONS.find((option) => option.value === sortOption)?.lable ||
+    SORT_OPTIONS[0].lable;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="cursor-pointer">
+        <Button variant="outline" className="w-full">
+          Sort by: {selectedSortLabel}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {SORT_OPTIONS.map((option) => (
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => onChange(option.value)}
+          >
+            {option.lable}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default SortOptionDropdown;
